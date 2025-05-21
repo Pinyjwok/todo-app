6c83cabbd88919f12b146be0237c95eb7ab37f7f
Pinyjwok <pinyjwok@example.com>
2025-05-21 18:56:33 +1000

    Refactor todo app structure and styling; implement core components for task management

---
+++ todo-app/src/components/TodoCard.jsx
@@ -0,0 +1,26 @@
+import { TodoList } from "./TodoList"
+
+export function TodoCard(props) {
+    const{ todo, handleDeleteTodo, todoIndex, handleCompleteTodo } = props
+
+
+
+    return (
+        <div className='card todo-item'>
+            <p>{todo.input}</p>
+            <div className='todo-buttons'>
+                <button onClick={() => {
+                    handleCompleteTodo(todoIndex)
+                    }}
+                      disabled={todo.complete}>
+                    <h6>Done</h6>
+                </button>
+                <button onClick={() => {
+                    handleDeleteTodo(todoIndex)
+                }}>
+                    <h6>Delete</h6>
+                </button>
+            </div>
+        </div>
+    )
+}  
